3a7f51bf26e8735673aed982a3dd1d6a9f8f70cc
Ayxan <ayxan.shahsuvarov1@gmail.com>
2022-04-15 17:46:18 +0400

    .

--- py/test.py
+++ py/test.py
@@ -1,32 +1,54 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn.tree import export_graphviz 
-from IPython.display import Image  
-import pydotplus
-from six import StringIO
+import numpy as np
+import matplotlib.pyplot as plt
 
-col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+def estimate_coef(x, y):
+	# number of observations/points
+	n = np.size(x)
 
-pima = pd.read_csv("C:\\Users\\shahs\\Documents\\test\\data\\diabetes.csv", header=None, names=col_names)[1:]
+	# mean of x and y vector
+	m_x = np.mean(x)
+	m_y = np.mean(y)
 
-# split dataset in features and target variable
-feature_cols = ['pregnant', 'insulin', 'bmi', 'age','glucose','bp','pedigree']
-x = pima[feature_cols] # Features
-y = pima.label # Target variable
+	# calculating cross-deviation and deviation about x
+	SS_xy = np.sum(y*x) - n*m_y*m_x
+	SS_xx = np.sum(x*x) - n*m_x*m_x
 
-# Split dataset into training set and test set
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
+	# calculating regression coefficients
+	b_1 = SS_xy / SS_xx
+	b_0 = m_y - b_1*m_x
 
-clf = DecisionTreeClassifier()
+	return (b_0, b_1)
 
-clf = clf.fit(x_train, y_train)
+def plot_regression_line(x, y, b):
+	# plotting the actual points as scatter plot
+	plt.scatter(x, y, color = "m",
+			marker = "o", s = 30)
 
-dot_data = StringIO()
-export_graphviz(clf, out_file=dot_data,  
-                filled=True, rounded=True,
-                special_characters=True,feature_names = feature_cols,class_names=['0','1'])
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
-graph.write_png('car.png')
-Image(graph.create_png())
+	# predicted response vector
+	y_pred = b[0] + b[1]*x
+
+	# plotting the regression line
+	plt.plot(x, y_pred, color = "g")
+
+	# putting labels
+	plt.xlabel('x')
+	plt.ylabel('y')
+
+	# function to show plot
+	plt.show()
+
+def main():
+	# observations / data
+	x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+	y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+
+	# estimating coefficients
+	b = estimate_coef(x, y)
+	print("Estimated coefficients:\nb_0 = {} \
+		\nb_1 = {}".format(b[0], b[1]))
+
+	# plotting regression line
+	plot_regression_line(x, y, b)
+
+if __name__ == "__main__":
+	main()
